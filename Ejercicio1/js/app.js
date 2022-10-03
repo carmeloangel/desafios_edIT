e156f3787ea53cbda8e2f1475909c5d7dc34f211
Carmelo <angel.o.cmp@gmail.com>
2022-10-03 18:09:07 -0300

    Se agrega lo pedido en el desafío del módulo 12. Y ya que son muy parecidos al challenge de la clase 12, se considera realizado con esos ejercicios.

--- Ejercicio1/js/app.js
+++ Ejercicio1/js/app.js
@@ -1,11 +1,39 @@
-let costoFijo = prompt("Ingrese el costo fijo: ");
+let day = new Date().getDay();
+let welcome = document.getElementById("welcome");
 
-let precioVenta = parseInt(prompt("Ingrese el precio de venta: "));
+switch (day) {
+  case 0:
+    mensaje = "Hoy es domingo. Que descanses muy bien!";
+    break;
+  case 1:
+    mensaje = "Es lunes! Arrancamos con toda!";
+    break;
+  case 2:
+    mensaje = "Que tengas lindo martes!";
+    break;
+  case 3:
+    mensaje = "Vamos que estamos en la mitad. Buen miercoles!";
+    break;
+  case 4:
+    mensaje = "Un casi viernes. Ten lindo jueves!";
+    break;
+  case 5:
+    mensaje = "Es hoy!! El ultimo envion y ya finde. Buen viernes!";
+    break;
+  case 6:
+    mensaje = "Tamos chelo y el cuerpo lo sabe. Que tengas sabadozo!";
+    break;
+  default:
+    mensaje = "Hubo un error al obtener el dia.";
+    break;
+}
 
-let costoVariable = parseInt(
-  prompt("Ingrese el costo letiable (debe ser menor que el precio de venta): ")
-);
+let age = Number(prompt("Ingresa tu edad: "));
 
-let puntoEquilibrio = costoFijo / (precioVenta - costoVariable);
-
-alert("El punto de equilibrio es: " + puntoEquilibrio);
+if (age >= 18) {
+  alert(mensaje + " Bienvenido!");
+  welcome.innerHTML = mensaje + " Bienvenido!";
+} else {
+  alert("Error. Aun no es mayor de edad. Acceso denegado.");
+  welcome.innerHTML = "Error. Aun no es mayor de edad. Acceso denegado.";
+}
--- Ejercicio2/js/app.js
+++ Ejercicio2/js/app.js
@@ -1,15 +1,41 @@
-let ingresoTotal = parseInt(prompt("Ingrese el total de los ingresos: "));
+let day = new Date().getDay();
+let welcome = document.getElementById("welcome");
 
-let gastosNecesarios = ingresoTotal * 0.5;
-let gastosOpcionales = ingresoTotal * 0.3;
-let ahorroInversion = ingresoTotal * 0.2;
+switch (day) {
+  case 0:
+    mensaje = "Hoy es domingo. Que descanses muy bien!";
+    break;
+  case 1:
+    mensaje = "Es lunes! Arrancamos con toda!";
+    break;
+  case 2:
+    mensaje = "Que tengas lindo martes!";
+    break;
+  case 3:
+    mensaje = "Vamos que estamos en la mitad. Buen miercoles!";
+    break;
+  case 4:
+    mensaje = "Un casi viernes. Ten lindo jueves!";
+    break;
+  case 5:
+    mensaje = "Es hoy!! El ultimo envion y ya finde. Buen viernes!";
+    break;
+  case 6:
+    mensaje = "Tamos chelo y el cuerpo lo sabe. Que tengas sabadozo!";
+    break;
+  default:
+    mensaje = "Hubo un error al obtener el dia.";
+    break;
+}
 
-alert(
-  "El dinero sera usado de la siguiente manera: $" +
-    gastosNecesarios +
-    " para gastos Necesarios. $" +
-    gastosOpcionales +
-    " para gastos Opcionales. $" +
-    ahorroInversion +
-    " para ahorro e inversion."
-);
+let user = prompt("Ingresa tu nombre de usuario: ");
+let password = Number(prompt("Ingresa tu contraseña: "));
+
+if (user === "admin" && password === 1234) {
+  alert(mensaje + " Bienvenido!");
+  welcome.innerHTML = mensaje + " Bienvenido!";
+} else {
+  alert("Error. Nombre de usuario o contraseña incorrectos. Acceso denegado.");
+  welcome.innerHTML =
+    "Error. Nombre de usuario o contraseña incorrectos. Acceso denegado.";
+}
