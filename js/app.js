1156aa4420eb3b1d2eaca2c88de9ba08dd97d370
Carmelo <angel.o.cmp@gmail.com>
2022-11-14 23:06:18 -0300

    Se agrega el desafío que modifica al original del módulo 17. Correspondiente a la clase del dia 01/08/2022

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-let calculator = function (number1, number2, operation) {
-  switch (operation) {
-    case "+":
-      return number1 + number2;
-    case "-":
-      return number1 - number2;
-    case "*":
-      return number1 * number2;
-    case "/":
-      return number1 / number2;
-    default:
-      console.log("Operación no valida.");
-      return NaN;
-  }
-};
+/* Cree una función llamada  function gastoConIva() que acepte un número y devuelva ese número más el 21% que es el valor del IVA. Luego crea una function map() que tome dos entradas o parámetros: <br>
+        1) Un array de como valores números. <br> 
+        2) Una función callback: esta función se aplica a cada elemento del array (dentro de la función map(). <br>
+Haga que su function map() devuelva una nueva matriz llena de números que son el resultado de usar la función callback en cada elemento de la matriz de entrada */
 
-console.log("Suma entre 3 y 6: " + calculator(3, 6, "+"));
-console.log("Resta entre 3 y 6: " + calculator(3, 6, "-"));
-console.log("Multiplicación entre 3 y 6: " + calculator(3, 6, "*"));
-console.log("División entre 3 y 6: " + calculator(3, 6, "/"));
+function gastoConIva(numero) {
+  return numero * 1.21;
+}
 
-console.log("División entre 3 y 6: " + calculator(3, 6, "p"));
+function map(array, callback) {
+  matrizIVA = [];
+  array.forEach((element) => {
+    matrizIVA.push([element, callback(element)]);
+  });
+  return matrizIVA;
+}
+
+array = [12000, 563, 5865, 166, 100];
+
+console.log(map(array, gastoConIva));
